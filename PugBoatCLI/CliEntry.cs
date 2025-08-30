81ba2d91a57e13857a84bc1760375099fbd8d5e2
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2025-08-30 22:49:55 +1000

    [proj] Separate core and add cli proj

---
+++ PugBoatCLI/CliEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace PugBoatCLI
+{
+    internal class CliEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ PugBoatCore/Core.cs
@@ -0,0 +1,7 @@
+﻿namespace PugBoatCore
+{
+    public class Core
+    {
+
+    }
+}
